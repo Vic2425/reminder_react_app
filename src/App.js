02377f5180c31dbe580e04725e3061ccf082fc29
Vic <vicjs612@gmail.com>
2023-06-06 18:30:29 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Router from "./Router";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
 import store from "./redux/store";
 
 function App() {
   return (
     <Provider store={store}>
+      <ToastContainer />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
